49d0ce82babab588db66654431a393dbeb95c4ee
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-12 14:49:13 -0800

    01. MoonShot: Introduction transcribed

--- 08_MoonShot/Day One Notes/01. Moonshot- Introduction.swift
+++ 08_MoonShot/Day One Notes/01. Moonshot- Introduction.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+//MARK: Moonshot: Intrduction
+
+//In this project we're goingot build an app that lets users learn about the missions and astronauts tat fromed NASA's Apollo space program. You'll get more experience with Codable, but more importantly you'll also work with scroll views, navigation, and much more interesting layouts.
+
+//Yes, you'll get some practice time with List, Text, and more, but youll also start to solve important SwiftUI problems - how can you make an image fit its space correctly? How can we clean up code using computed properties? how can we compose smaller views into larger ones to help keep our project organized?
+
+//As always there's lots to do, so let's get started: create a new iOS app using the Single View App template, naming it "Moonshot". We'll be using that for the project, but first lets take a closer look at the new techniques you'll need to become familiar with...
