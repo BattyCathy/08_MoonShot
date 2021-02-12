5eb31082f6fab75e97fbe6cf818a980673d05fbc
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-12 14:38:46 -0800

    TOC transcribed

---
+++ 08_MoonShot/Day One Notes/Project 8, Part One.swift
@@ -0,0 +1,30 @@
+//
+//  Project 8, Part One.swift
+//  08_MoonShot
+//
+//  Created by Austin Roach on 2/12/21.
+//
+
+import Foundation
+
+//MARK: Project 8, Part One
+
+//As I write these words, today is the dat Apple TV+ launches - a new subscriptions service that promises high-quality steaming television with a whole raft of original content.
+
+//In my filter bubble, the show eveyone is talking about isi "For All Mankind" that dramatizes an alternative history of the 1969 moon landing. So, I figured today would be a good day to start a SwiftUI project called Moonshow, detailing some of the history behind Nasa's Apollo space program.
+
+//I also think it would be fitting if today's quote came from Neil Armstrong, the first person to walk on the moon. Back in 2000 he said, "science is about what is; engineering is about what can be." I don't know about you, but I find that hugely inspiring: every time we create a new Xcode project we have a blank slate to work with, and that can be whatever we want.
+
+//Today we're learning about the techniques to build Moonshow, but as with all the techniques we're learning they formpar of your larger Knowledge for you to mix and remix as you please for years to come.
+
+//Toda you have five topics to work through, in which you'll learn about GeometryReady, ScrollView, NavigationLink, and more.
+
+//--Moonshot: Introduction
+
+//--Resisizing Images to Fit the Screen Using GeometryReader
+
+//--Hide ScrollView Lets Us Work with Scrolling Data
+
+//--Working with Hierarchincal Codable Data
+
+//Don't forget to post your progress somewhere - stay accountable! (And when you're done, sit down and watch some For All Mankind.)
