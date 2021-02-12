ec32fdee0c5575185c445a518b6e30e92b09c756
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-12 12:30:40 -0800

    Initial Commit

---
+++ 08_MoonShot/_8_MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8_MoonShotApp.swift
+//  08_MoonShot
+//
+//  Created by Austin Roach on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct _8_MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
